6bb4865577b13847801d80a80fe767d75da6ac2c
chrismountzouris <30795982+chrismountzouris@users.noreply.github.com>
2021-02-07 21:48:23 +0200

    Partial commit of input process
    Partial commit of input process

--- appendRow.py
+++ appendRow.py
@@ -1,5 +1,45 @@
 import pygsheets
 
+def validate_age(age):
+
+    if (age.isnumeric() == True):
+
+        if (int(age) > 18 and int(age) < 80): return True
+
+        else : return False
+
+    else: return False
+
+def validate_weight(weight):
+
+    if (weight.isnumeric() == True):
+
+        if (int(weight) > 30 and int(weight) < 180): return True
+
+        else : return False
+
+    else: return False
+
+def validate_height(height):
+
+    if (height.isnumeric() == True):
+
+        if (int(height) > 130 and int(height) < 230): return True
+
+        else : return False
+
+    else: return False
+
+def validate_sex(sex):
+
+    if (sex.isnumeric() == True):
+
+        if (int(sex) == 1 or int(sex) == 2): return True
+
+        else : return False
+
+    else: return False
+
 def auth_gsheets():
 
     service_file = r'pmvcalculator-304117-16a25badba46.json'
@@ -26,6 +66,46 @@ def append_gsheets():
 
     return None
 
+while True :
+
+    age = input ("Please enter your age:")
+
+    valid = validate_age(age)
+
+    if valid == True : break
+
+    else : print ("INVALID")
+
+while True :
+
+    weight = input ("Please enter your weight [kg]:")
+
+    valid = validate_weight(weight)
+
+    if valid == True : break
+
+    else : print ("INVALID")
+
+while True :
+
+    height = input ("Please enter your height [cm]:")
+
+    valid = validate_height(height)
+
+    if valid == True : break
+
+    else : print ("INVALID")
+
+while True :
+
+    sex = input ("Please enter your sex [1: Male, 2: Female]:")
+
+    valid = validate_sex(sex)
+
+    if valid == True : break
+
+    else : print ("INVALID")
+
 wks = auth_gsheets()
 
-append_gsheets()
+#append_gsheets()
